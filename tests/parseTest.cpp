4b9cc598f0e3c87dc579644b7c248ab0b283437e
Fernando Welzel <fernando.welzel.br@gmail.com>
2023-10-28 20:20:43 +0200

    Changing testing framework - Google test

--- tests/parseTest.cpp
+++ tests/parseTest.cpp
@@ -1,16 +1,33 @@
 #include "parse.hpp"
+#include <gtest/gtest.h>
 
 #include <string>
-#include <cassert>
 
-int main(int argc, char **argv) {
+// Simple test - Try to parse a TYPE
+TEST(parseTest, parseType)
+{
+    string description, value;
 
-   string description, value;
+    parseLine("TYPE: CPU", description, value);
 
-   parseLine("TYPE: CPU", description, value);
+    ASSERT_EQ(description == "TYPE", 1) << "Type description not parsed correctly";
+    ASSERT_EQ(value == "CPU", 1) << "Type label not parsed correctly";
+}
 
-   assert(description == "TYPE");
-   assert(value == "CPU");
+// Simple test - Try to parse a LABEL with spaces
+TEST(parseTest, parseLabel)
+{
+    string description, value;
 
-   return 0;
+    parseLine("LABEL: My bus 1", description, value);
+
+    ASSERT_EQ(description == "LABEL", 1) << "Label description not parsed correctly";
+    ASSERT_EQ(value == "My bus 1", 1) << "Label value not parsed correctly";
+}
+
+// Runs all TEST functions declared at this file
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
