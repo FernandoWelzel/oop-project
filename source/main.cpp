e3c690a4b31b6a392db9e11406f84888e94e4539
Fernando Welzel <fernando.welzel.br@gmail.com>
2023-11-04 23:37:08 +0100

    Updated main to support options

--- source/main.cpp
+++ source/main.cpp
@@ -1,24 +1,62 @@
 #include <iostream>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "platform.hpp"
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    // TODO: Add option support
-    if(argc != 2) {
-        cerr << "Wrong number of arguments - Received: " << argc << " Expecting: 1" << endl;
+void printUsage(char* argv0) {
+    cout << "Usage: " << argv0 << " [options] filename" << endl;
+    cout << "Options:" << endl;
+    cout << "  -h  Display this help message" << endl;
+    cout << "  -v  Components print message each time they simulates" << endl;
+}
+
+int main(int argc, char* argv[]) {
+    int option;
+
+    // Default values for options
+    int helpFlag = 0;
+    int verboseFlag = 0;
+
+    // Parse command-line options using getopt
+    while ((option = getopt(argc, argv, "hv")) != -1) {
+        switch (option) {
+            case 'v':
+                verboseFlag = 1;
+                break;
+
+            case 'h':
+                helpFlag = 1;
+                break;
+
+            default:
+                // Printing error/help message
+                cout << endl;
+                printUsage(argv[0]);
+                
+                cout << endl;
+                cerr << "ERROR: Unknown or malformated option" << endl;
+
+                exit(1);
+        }
+    }
+
+    // Prints help if necessary
+    if (helpFlag) {
+        printUsage(argv[0]);
+        return 0;
     }
 
     // Constructing platform from file
-    Platform myPlatform(argv[1]);
+    Platform myPlatform(argv[argc-1]);
     
     // Binding components of platform
-    myPlataform.build();
+    myPlatform.build();
 
     // Simulate all components
-    myPlataform.simulate();
+    myPlatform.simulate();
     
     return 0;
 }
